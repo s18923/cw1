9b076a18c76308f2ee82cdc68800870582f8c09a
s18923 <s18923@pjwstk.edu.pl>
2020-03-05 13:33:27 +0100

    Commit 2

--- cw1/cw1/Program.cs
+++ cw1/cw1/Program.cs
@@ -10,21 +10,29 @@ namespace cw1
         static async Task Main(string[] args)
         {
             string url = args[0];
-            var client = new HttpClient();
-            var result = await client.GetAsync(url);
 
-            if (!result.IsSuccessStatusCode) return;
-
-            string html = await result.Content.ReadAsStringAsync();
-            Regex regex = new Regex(@"\b\w*[0-9]*@\b[\w,\.]*");
-            MatchCollection matches = regex.Matches(html);
-
-            Console.WriteLine(matches.Count);
-            Console.WriteLine("");
-
-            for (int i = 0; i < matches.Count; i++)
+            try
             {
-                Console.WriteLine(matches[i]);
+                var client = new HttpClient();
+                var result = await client.GetAsync(url);
+
+                if (!result.IsSuccessStatusCode) return;
+
+                string html = await result.Content.ReadAsStringAsync();
+                Regex regex = new Regex(@"\b\w*[0-9]*@\b[\w,\.]*");
+                MatchCollection matches = regex.Matches(html);
+
+                Console.WriteLine(matches.Count);
+                Console.WriteLine("");
+
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    Console.WriteLine(matches[i]);
+                }
+            } catch (Exception exc)
+            {
+                //string s = string.Format("Wystapil blad {0}", exc.ToString());
+                Console.WriteLine("Wystapil blad" + exc.ToString());
             }
         }
     }
