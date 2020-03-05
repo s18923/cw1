a51802b54e4b84a2205499d896bc457e96f2676b
Paweł Gut <s18923@pjwstk.edu.pl>
2020-03-05 12:57:29 +0100

    Pierwszy commit

---
+++ cw1/cw1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace cw1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = args[0];
+            var client = new HttpClient();
+            var result = await client.GetAsync(url);
+
+            if (!result.IsSuccessStatusCode) return;
+
+            string html = await result.Content.ReadAsStringAsync();
+            Regex regex = new Regex(@"\b\w*[0-9]*@\b[\w,\.]*");
+            MatchCollection matches = regex.Matches(html);
+
+            Console.WriteLine(matches.Count);
+            Console.WriteLine("");
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                Console.WriteLine(matches[i]);
+            }
+        }
+    }
+}
